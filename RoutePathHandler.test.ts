3b5e855ab808243ee21be2c3890f5241d43b8130
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-06 19:22:08 +0900

    Refactor the RoutePathHandler and add tests

---
+++ RoutePathHandler.test.ts
@@ -0,0 +1,110 @@
+import { assertEquals } from 'https://deno.land/std@0.100.0/testing/asserts.ts';
+import RoutePathHandler from './RoutePathHandler.ts';
+
+Deno.test('empty handler', () => {
+  const pathHandler = new RoutePathHandler();
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), undefined);
+});
+
+Deno.test('handler with /', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/');
+  assertEquals(pathHandler.findMatch('/'), '/');
+  assertEquals(pathHandler.findMatch('/users'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), undefined);
+});
+
+Deno.test('handler with /users', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/users');
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), '/users');
+  assertEquals(pathHandler.findMatch('/users/Alice'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), undefined);
+});
+
+Deno.test('handler with /users/:userId', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/users/:userId');
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice'), '/users/:userId');
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), undefined);
+});
+
+Deno.test('handler with /users/:userId/friends', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/users/:userId/friends');
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), '/users/:userId/friends');
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), undefined);
+});
+
+Deno.test('handler with /users/:userId/friends/:friendId', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/users/:userId/friends/:friendId');
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+  assertEquals(pathHandler.findMatch('/users/Alice/friends/Bob'), '/users/:userId/friends/:friendId');
+});
+
+Deno.test('handler ignores trailing /', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/');
+  pathHandler.addRoutePath('/users');
+  pathHandler.addRoutePath('/users/:userId');
+  assertEquals(pathHandler.findMatch('/'), '/');
+  assertEquals(pathHandler.findMatch('//'), '/');
+  assertEquals(pathHandler.findMatch('/users'), '/users');
+  assertEquals(pathHandler.findMatch('/users/'), '/users');
+  assertEquals(pathHandler.findMatch('/users/Alice'), '/users/:userId');
+  assertEquals(pathHandler.findMatch('/users/Alice/'), '/users/:userId');
+  assertEquals(pathHandler.findMatch('/users/Alice/friends'), undefined);
+});
+
+Deno.test('handler ignores multiple leading /', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/');
+  pathHandler.addRoutePath('/users');
+  pathHandler.addRoutePath('/users/:userId');
+  assertEquals(pathHandler.findMatch('/'), '/');
+  assertEquals(pathHandler.findMatch('//'), '/');
+  assertEquals(pathHandler.findMatch('///users'), '/users');
+  assertEquals(pathHandler.findMatch('///users/Alice'), '/users/:userId');
+  assertEquals(pathHandler.findMatch('////users/Alice/friends'), undefined);
+});
+
+Deno.test('handler recognizes empty paths', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/');
+  pathHandler.addRoutePath('/users');
+  pathHandler.addRoutePath('/users/:userId');
+  assertEquals(pathHandler.findMatch('/'), '/');
+  assertEquals(pathHandler.findMatch('/users/'), '/users');
+  assertEquals(pathHandler.findMatch('/users/Alice/'), '/users/:userId');
+  assertEquals(pathHandler.findMatch('/users//'), '/users/:userId');
+});
+
+Deno.test('handler prioritizes route path registered earliest', () => {
+  const pathHandler = new RoutePathHandler();
+  pathHandler.addRoutePath('/users');
+  pathHandler.addRoutePath('/:id');
+  pathHandler.addRoutePath('/:id2');
+  assertEquals(pathHandler.findMatch('/'), undefined);
+  assertEquals(pathHandler.findMatch('/users'), '/users');
+  for (let i = 0; i < 100; i++) {
+    assertEquals(pathHandler.findMatch('/user'), '/:id');
+  }
+});
--- RoutePathHandler.ts
+++ RoutePathHandler.ts
@@ -3,11 +3,17 @@
 /**
  * A handler that stores different route paths registered by Kyuko,
  * and offers methods to match url paths to those route paths.
+ *
+ * Note on handling slashes:
+ *   - Recurring leading slashes will be merged and considered as one slash
+ *   - Recurring slashes that appear mid-path will contribute to empty paths
+ *   - A single trailing slash will be ignored
+ *
+ * For more details, see: https://datatracker.ietf.org/doc/html/rfc3986.
  */
 export default class RoutePathHandler {
-
-  // dummy root node for route path tree
-  #rootNode = new RoutePathNode('\0', null);
+  #treeHeight = 0;
+  #rootNode = RoutePathNode.createRoot();
 
   /**
    * Adds a route path to the handler.
@@ -15,30 +21,12 @@ export default class RoutePathHandler {
    *
    * @param path A valid Kyuko route path such as "/", "/users", "/users/:id"
    */
-  addRoutePath(routePath: string) {
-    const strings = routePath.split('/');
-
-    // Add the nodes of the route path to the tree
+  addRoutePath(routePath: string): void {
+    const segments = this.splitPathSegments(routePath);
+    this.#treeHeight = Math.max(segments.length, this.#treeHeight);
     let currNode = this.#rootNode;
-    strings.forEach((str) => {
-      const newNode = new RoutePathNode(str, currNode);
-
-      // If wildcard
-      if (str.startsWith(':')) {
-        if (!currNode.wildcardChildren.has(str)) {
-          currNode.wildcardChildren.set(str, newNode);
-        }
-
-        currNode = currNode.wildcardChildren.get(str) as RoutePathNode;
-
-      // Not wildcard
-      } else {
-        if (!currNode.concreteChildren.has(str)) {
-          currNode.concreteChildren.set(str, newNode);
-        }
-
-        currNode = currNode.concreteChildren.get(str) as RoutePathNode;
-      }
+    segments.forEach((segment) => {
+      currNode = currNode.findOrCreateChild(segment);
     });
 
     currNode.isStationaryNode = true;
@@ -47,92 +35,175 @@ export default class RoutePathHandler {
   /**
    * Returns a route path that matches the input urlPath.
    * Returns undefined if no such path exists.
+   * Prioritizes route paths that were added earlier.
    *
    * @param urlPath The path to match.
    * @returns matched path if exists. undefined if not.
    */
-  findMatch(urlPath: string) {
-    const strings = urlPath.split('/');
-
-    // Trailing slashes will be ignored, except for the root path ("/")
-    if (urlPath.length > 2 && urlPath.endsWith('/')) {
-      strings.pop();
+  findMatch(urlPath: string): string | undefined {
+    const segments = this.splitPathSegments(urlPath);
+    if (this.#treeHeight < segments.length) {
+      return undefined;
     }
 
     let currNodes = [this.#rootNode];
-    for (const str of strings) {
+    for (const segment of segments) {
       if (currNodes.length === 0) {
         return undefined;
       }
 
       const nextNodes: RoutePathNode[] = [];
-      currNodes.forEach((currNode) => {
-        if (currNode.concreteChildren.has(str)) {
-          nextNodes.push(currNode.concreteChildren.get(str) as RoutePathNode);
-        }
-
-        currNode.wildcardChildren.forEach((wildcardNode) => {
-          nextNodes.push(wildcardNode);
-        });
+      currNodes.forEach((node) => {
+        nextNodes.push(...node.findMatchingChildren(segment));
       });
 
       currNodes = nextNodes;
     }
 
-    // Prioritize route paths that were added first
-    const matchedNode = currNodes.filter(node => node.isStationaryNode)[0];
-    if (matchedNode === undefined) {
+    const matchedNodes = currNodes.filter(node => node.isStationaryNode);
+    if (matchedNodes.length === 0) {
       return undefined;
     }
 
-    return RoutePathNode.getFullPath(matchedNode);
+    return matchedNodes[0].getFullPath();
+  }
+
+  /**
+   * Splits the given path into an array of path segments.
+   * Note that `splitPathSegments(path).join('/') !== path`.
+   *
+   * Examples:
+   *   - `'/'`           => `['']`
+   *   - `'//'`          => `['']`
+   *   - `'/users'`      => `['', 'users']`
+   *   - `'//users'`     => `['', 'users']`
+   *   - `'/users/'`     => `['', 'users']`
+   *   - `'/users/:id'`  => `['', 'users', ':id']`
+   *   - `'/users//:id'` => `['', 'users', '', ':id']`
+   *
+   * @param path The route or url path to split
+   */
+  private splitPathSegments(path: string): string[] {
+    const result = path.split('/');
+    const divider = result.findIndex(seg => seg !== '');
+    if (divider === -1) {
+      return [''];
+    }
+
+    result.splice(0, divider - 1);
+    if (result[result.length - 1] === '') {
+      result.pop();
+    }
+
+    return result;
   }
 }
 
 /**
- * Represents a "node" of a route path.
- * For example, ["", "users", ":id"] are nodes of the route path "/users/:id".
+ * Represents a segment of a route path as a tree node.
+ * For example, ["", "users", ":id"] are segments of the route path "/users/:id".
+ * Each segment of the route path will be stored in a tree data structure as nodes.
  */
 class RoutePathNode {
 
-  // The string value of the node
-  value: string;
-
-  // The parent of the node
-  parent: RoutePathNode | null;
-
-  // Whether the node can be considered the end of a path
+  /**
+   * Whether the node can be considered the end of a path or not.
+   */
   isStationaryNode: boolean;
 
-  // Direct children of the node, where their values are not wildcard values
-  concreteChildren: Map<string, RoutePathNode>;
+  #value: string;
+  #parent: RoutePathNode | null;
+  #concreteChildren: Map<string, RoutePathNode>;
+  #wildcardChildren: Map<string, RoutePathNode>;
 
-  // Direct children of the node, where their values are wildcard values
-  wildcardChildren: Map<string, RoutePathNode>;
-
-  constructor(value: string, parent: RoutePathNode | null) {
-    this.value = value;
-    this.parent = parent;
-    this.isStationaryNode = false;
-    this.concreteChildren = new Map();
-    this.wildcardChildren = new Map();
+  /**
+   * @returns A new `RoutePathNode` that acts as the root of the tree.
+   */
+  static createRoot(): RoutePathNode {
+    return new RoutePathNode('\0', null);
   }
 
   /**
-   * Traverses through the parents of the node and constructs the full path
-   * that the node represents.
+   * Constructs a `RoutePathNode` object.
+   * Private constructor to prevent clients from creating circular trees.
+   * Use `RoutePathNode.createRoot()` to create root nodes.
    *
-   * @param node The final node of the path.
-   * @returns The full path
+   * @param value The value of the segment of the route path. '\0' for root node.
+   * @param parent The parent of the segment of the route path. null for root node.
    */
-  static getFullPath(node: RoutePathNode) {
-    const result = [];
-    let currNode = node;
-    while (currNode.parent !== null) {
-      result.unshift(currNode.value);
-      currNode = currNode.parent;
+  private constructor(value: string, parent: RoutePathNode | null) {
+    this.isStationaryNode = false;
+    this.#value = value;
+    this.#parent = parent;
+    this.#concreteChildren = new Map();
+    this.#wildcardChildren = new Map();
+  }
+
+  /**
+   * Finds and returns a child node that contains the **exact** `value`.
+   * If the child doesn't exist, adds a new child node and returns that.
+   *
+   * @param value The value of the child node to find or create.
+   * @returns The child node with the corresponding `value` (could be newly-created).
+   */
+  findOrCreateChild(value: string): RoutePathNode {
+    let container = this.#concreteChildren;
+    if (value.startsWith(':')) {
+      container = this.#wildcardChildren;
     }
 
-    return result.join('/');
+    if (container.has(value)) {
+      return container.get(value) as RoutePathNode;
+    }
+
+    const newNode = new RoutePathNode(value, this);
+    container.set(value, newNode);
+    return newNode;
+  }
+
+  /**
+   * Finds and returns children that match the `value`, including wildcards.
+   *
+   * @param value The value of the child node to match.
+   * @returns An array of children that matches the `value`, including wildcards
+   */
+  findMatchingChildren(value: string): RoutePathNode[] {
+    const result: RoutePathNode[] = [];
+    if (this.#concreteChildren.has(value)) {
+      result.push(this.#concreteChildren.get(value) as RoutePathNode);
+    }
+
+    this.#wildcardChildren.forEach((child) => {
+      result.push(child);
+    });
+
+    return result;
+  }
+
+  /**
+   * Traverses through the parents of the node
+   * and constructs the full route path that the node represents.
+   *
+   * @returns The full route path.
+   */
+  getFullPath(): string {
+    /**
+     * Time complexity: O(n)
+     *
+     * We don't use result.unshift() because
+     *   - while loop: O(n)
+     *   - Array.prototype.push(): O(1)
+     *   - Array.prototype.unshift(): O(n)
+     *   - Array.prototype.reverse(): O(n)
+     */
+    const result = [];
+    let node = this as RoutePathNode;
+    while (node.#parent !== null) {
+      result.push(node.#value);
+      node = node.#parent;
+    }
+
+    result.reverse();
+    return result.join('/') || '/';
   }
 }
