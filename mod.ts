6ecd17ee90814fd29c1399dc02540b6fe109bb1b
Riki Khorana <rskhorana@gmail.com>
2021-07-08 21:24:47 +0900

    Separate interface and implementation (#13)
    * Separate interface and implementation for objects
    
    * Remove extra line

--- mod.ts
+++ mod.ts
@@ -1,10 +1,6 @@
 // Copyright 2021 Riki Singh Khorana. All rights reserved. MIT license.
 
-export { Kyuko } from "./kyuko.ts";
-export type {
-  KyukoMiddleware,
-  KyukoRequest,
-  KyukoRequestHandler,
-} from "./kyuko.ts";
-
-export { KyukoResponse } from "./KyukoResponse.ts";
+export { Kyuko } from "./src/Kyuko.ts";
+export type { KyukoMiddleware, KyukoRequestHandler } from "./src/Kyuko.ts";
+export type { KyukoRequest } from "./src/KyukoRequest.ts";
+export type { KyukoResponse } from "./src/KyukoResponse.ts";
