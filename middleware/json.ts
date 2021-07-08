ec408c6ccfe07d5af9f90343b7b1d77356a1fda8
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-08 22:31:29 +0900

    Implement decodeParams, and modify json middleware

--- middleware/json.ts
+++ middleware/json.ts
@@ -1,16 +1,32 @@
-import { KyukoRequest, KyukoResponse } from "../mod.ts";
+import { KyukoMiddleware, KyukoRequest } from "../mod.ts";
 
 export interface KyukoRequestWithJson extends KyukoRequest {
   // deno-lint-ignore no-explicit-any
   requestBody: any;
 }
 
-export async function json(req: KyukoRequest, res: KyukoResponse) {
-  const contentType = req.headers.get("content-type");
-  res.headers.append("Content-Type", "application/json; charset=utf-8");
-  if (contentType?.includes("application/json")) {
-    const requestClone = req.clone();
-    const json = await requestClone.json();
-    (req as KyukoRequestWithJson).requestBody = json;
-  }
+/**
+ * Returns a `KyukoMiddleware` that attempts to parse the request body as JSON.
+ * The parsed body is stored into `req.requestBody`.
+ * Note that `req.body` will be stay unused (hence `req.bodyUsed === false`).
+ *
+ * example:
+ *
+ * ```ts
+ * app.use(json());
+ *
+ * app.post("/", (req, res) => {
+ *   const { requestBody } = req as KyukoRequestWithJson;
+ * });
+ * ```
+ */
+export function json(): KyukoMiddleware {
+  return async function (req: KyukoRequest) {
+    const contentType = req.headers.get("content-type");
+    if (contentType?.includes("application/json")) {
+      const requestClone = req.clone();
+      const json = await requestClone.json();
+      (req as KyukoRequestWithJson).requestBody = json;
+    }
+  };
 }
