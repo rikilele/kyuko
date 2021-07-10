ea894f01e6ddc9971d465ce770aea3e74e46f72a
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-10 17:22:08 +0900

    Improve documentation on Kyuko class

--- src/Kyuko.ts
+++ src/Kyuko.ts
@@ -40,6 +40,46 @@ export type KyukoErrorHandler = (
 /**
  * An ultra-light framework for API servers hosted on [Deno Deploy](https://deno.com/deploy).
  * Aims to provide a similar experience to developing API servers with [Express](https://expressjs.com/).
+ *
+ * **Route methods**
+ *
+ * Kyuko currently supports the following HTTP methods:
+ *
+ * * GET
+ * * POST
+ * * PUT
+ * * DELETE
+ * * PATCH
+ * * HEAD
+ *
+ * **Route paths**
+ *
+ * Route paths define endpoints at which requests can be made.
+ *
+ * > Kyuko internally uses the [`URL.pathname`](https://developer.mozilla.org/en-US/docs/Web/API/URL/pathname)
+ * > property to match url paths with route paths.
+ *
+ * Route paths consist of segments that can either be concrete or a wildcard.
+ * In the following example, `users` is a concrete segment, while `:userId` is a wildcard segment.
+ * The example will handle requests that are sent to "/users/Alice", "/users/Bob", etc.
+ *
+ * ```ts
+ * app.get("/users/:userId", (req, res) => {
+ *   const { userId } = req.params;
+ *   res.send(`Hello ${userId}!`);
+ * });
+ * ```
+ *
+ * Kyuko only officially supports route paths that consist of unreserved characters
+ * [RFC3986 section 2.3](https://datatracker.ietf.org/doc/html/rfc3986#section-2.3).
+ * The behavior for when a route path consisting of other characters is registered is undefined.
+ *
+ * Note on handling of slashes in paths:
+ * * Recurring leading slashes will be merged and considered as one slash
+ * * Recurring slashes that appear mid-path will contribute to empty paths
+ * * A single trailing slash will be ignored
+ *
+ * For more details, read more on [RFC3986](https://datatracker.ietf.org/doc/html/rfc3986).
  */
 export class Kyuko {
   #routes;
@@ -50,13 +90,6 @@ export class Kyuko {
 
   /**
    * Initializes a new Kyuko app.
-   * Supports routing for the following methods as of now:
-   *   - GET
-   *   - POST
-   *   - PUT
-   *   - DELETE
-   *   - PATCH
-   *   - HEAD
    */
   constructor() {
     this.#routes = new RoutePathHandler();
@@ -178,15 +211,6 @@ export class Kyuko {
   }
 
   /**
-   * Starts listening to 'fetch' requests.
-   * @param callback Called when server starts listening.
-   */
-  listen(callback?: VoidFunction) {
-    addEventListener("fetch", this.handleFetchEvent.bind(this));
-    callback && callback();
-  }
-
-  /**
    * Adds `middleware` to a list of application-level middleware to run.
    * Middleware are invoked in order of addition via `use()`.
    */
@@ -202,6 +226,15 @@ export class Kyuko {
    */
   error(errorHandler: KyukoErrorHandler) {
     this.#errorHandlers.push(errorHandler);
+  }
+
+  /**
+   * Starts listening to 'fetch' requests.
+   * @param callback Called when server starts listening.
+   */
+  listen(callback?: VoidFunction) {
+    addEventListener("fetch", this.handleFetchEvent.bind(this));
+    callback && callback();
   }
 
   private handleFetchEvent(event: FetchEvent) {
