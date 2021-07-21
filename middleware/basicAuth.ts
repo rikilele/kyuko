16454e5a5be017528fb971cee99bc56bf99a406b
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-21 22:04:52 +0900

    Create separate deps.ts for middleware

--- middleware/basicAuth.ts
+++ middleware/basicAuth.ts
@@ -1,6 +1,6 @@
 // Copyright 2021 Riki Singh Khorana. All rights reserved. MIT license.
 
-import { Status } from "../src/deps.ts";
+import { Status } from "./deps.ts";
 import { KyukoMiddleware, KyukoRequest, KyukoResponse } from "../mod.ts";
 
 /**
