7f7b937960151ef7e27fafb92397af9fcc6a2ae5
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-22 18:01:52 +0900

    Prevent serveStatic from proxying headers

--- middleware/serveStatic.ts
+++ middleware/serveStatic.ts
@@ -5,18 +5,16 @@ import { KyukoMiddleware, KyukoRequest, KyukoResponse } from "../mod.ts";
 
 /**
  * Returns a `KyukoMiddleware` that serves static assets.
- * The middleware will proxy files located under ``${url}${dir}`` when a request
- * is made to `path`. If you wish to serve static files that are hosted
+ * The middleware will proxy files located under the `dir` at `url` when a
+ * request is made to `path`. If you wish to serve static files that are hosted
  * alongside your source code (the Kyuko app), you should set `url` to
- * `import.meta.url`.
+ * `import.meta.url`. Note that you must add a trailing slash "/" to `url` when
+ * specifying a remote url other than `import.meta.main`.
  *
  * ```ts
  * // static assets placed alongside the app will be served
  * app.use(serveStatic(import.meta.url));
  * ```
- *
- * Note that some files may not be served properly if `url` is set to external
- * addresses, mainly due to client-side resource policies.
  *
  * @param url The url that the static assets are located at.
  * @param dir The directory under the url that the static assets are located at. Default is ".".
@@ -33,16 +31,12 @@ export function serveStatic(
       const fileName = req.path.split("/").at(-1) as string;
       const contentTypeHeader = contentType(fileName);
 
-      // contentTypeHeader = undefined if requested file can't be served
+      // contentTypeHeader = undefined if file can't be served statically
       if (contentTypeHeader) {
-        const fileUrl = new URL(`${dir}${req.path}`, url);
+        const fileUrl = new URL(dir + req.path, url);
         const file = await fetch(fileUrl);
         if (file.ok && !res.wasSent()) {
-          // Set content-type header
-          res.headers = new Headers(file.headers);
           res.headers.set("content-type", contentTypeHeader);
-
-          // Set body and send
           res.body = file.body;
           res.status(file.status).send();
         }
