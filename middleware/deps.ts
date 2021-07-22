e1411887349989269ac0d22e405e4701818d4c34
Riki Khorana <riki.singh.khorana@sony.com>
2021-07-22 16:19:34 +0900

    Add content-type header to serveStatic middleware

--- middleware/deps.ts
+++ middleware/deps.ts
@@ -1,1 +1,2 @@
 export { Status } from "https://deno.land/std@0.102.0/http/http_status.ts";
+export { contentType } from "https://deno.land/x/media_types@v2.9.3/mod.ts";
--- middleware/serveStatic.ts
+++ middleware/serveStatic.ts
@@ -1,18 +1,22 @@
 // Copyright 2021 Riki Singh Khorana. All rights reserved. MIT license.
 
+import { contentType } from "./deps.ts";
 import { KyukoMiddleware, KyukoRequest, KyukoResponse } from "../mod.ts";
 
 /**
  * Returns a `KyukoMiddleware` that serves static assets.
  * The middleware will proxy files located under ``${url}${dir}`` when a request
- * is made to ``${path}/:asset``. If you wish to serve static files that are
- * hosted alongside your source code (the Kyuko app), you should set
- * `url` to `import.meta.url`.
+ * is made to `path`. If you wish to serve static files that are hosted
+ * alongside your source code (the Kyuko app), you should set `url` to
+ * `import.meta.url`.
  *
  * ```ts
  * // static assets placed alongside the app will be served
  * app.use(serveStatic(import.meta.url));
  * ```
+ *
+ * Note that some files may not be served properly if `url` is set to external
+ * addresses, mainly due to client-side resource policies.
  *
  * @param url The url that the static assets are located at.
  * @param dir The directory under the url that the static assets are located at. Default is ".".
@@ -26,12 +30,22 @@ export function serveStatic(
 ): KyukoMiddleware {
   return async function serveStatic(req: KyukoRequest, res: KyukoResponse) {
     if (req.path.startsWith(path)) {
-      const fileUrl = new URL(`${dir}${req.path}`, url);
-      const file = await fetch(fileUrl);
-      if (file.ok && !res.wasSent()) {
-        res.body = file.body;
-        res.headers = file.headers;
-        res.status(file.status).send();
+      const fileName = req.path.split("/").at(-1) as string;
+      const contentTypeHeader = contentType(fileName);
+
+      // contentTypeHeader = undefined if requested file can't be served
+      if (contentTypeHeader) {
+        const fileUrl = new URL(`${dir}${req.path}`, url);
+        const file = await fetch(fileUrl);
+        if (file.ok && !res.wasSent()) {
+          // Set content-type header
+          res.headers = new Headers(file.headers);
+          res.headers.set("content-type", contentTypeHeader);
+
+          // Set body and send
+          res.body = file.body;
+          res.status(file.status).send();
+        }
       }
     }
   };
